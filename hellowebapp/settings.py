2cd9a3e729d293ebf45c3188c085aeb5c6d3b403
Bradley Shively <bshively@gmail.com>
2015-09-28 02:42:52 +0000

    Configuration changes to prep for Heroku

--- hellowebapp/settings.py
+++ hellowebapp/settings.py
@@ -85,6 +85,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Registration
 ACCOUNT_ACTIVATION_DAYS = 7
